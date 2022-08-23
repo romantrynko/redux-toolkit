13f70b4f5c1107fe155f9c41f9f95c89e92a5807
Roman Trynko <trynkoroman25@gmail.com>
2022-08-23 12:24:23 +0300

    init commit

---
+++ components/Users.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+export default function Clock() {
+  const [name, setName] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const addNewUser = () => {
+    setUsers([...users, user]);
+  };
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <div>
+      <label>New User:</label>
+      <input type="text" value={name} onChange={handleChange} />
+      <button onClick={addNewUser}>Add</button>
+      <h4>User List:</h4>
+      <ol>
+        {users.map((user, index) => (
+          <li key={index}>{user}</li>
+        ))}
+      </ol>
+    </div>
+  );
+}
